49efa72fb784528bf5964bbdc49c305cde778a3e
fanwenxiang <fanwenxiang@aiforward.com>
2021-11-04 11:42:38 +0800

    feature:修改配置

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -3,9 +3,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 const { VueLoaderPlugin } = require('vue-loader');
 const webpack = require('webpack');
@@ -13,6 +15,8 @@ const webpack = require('webpack');
 function resolve(p) {
   return path.resolve(__dirname, '..', p)
 }
+
+const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
   performance: {
@@ -24,6 +28,14 @@ module.exports = {
     app: './src/main.js'
     // vendor: ['vue', 'vue-router', 'vuex', 'element-ui', 'lodash']
   },
+  output: {
+    filename: NODE_ENV === 'production' ? 'static/js/[name].[contenthash:8].js' : 'static/js/[name].js' ,
+    path: resolve('dist'),
+    publicPath: '/',
+    chunkFilename: 'static/js/[name].[contenthash:8].js',
+    assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  devtool: NODE_ENV === 'production' ? false : 'source-map',
   resolve: {
     extensions: ['.js', '.vue'],  
     alias: {
@@ -76,22 +88,10 @@ module.exports = {
       },
       { // 加载图片
         test: /\.(png|svg|jpg|jpeg|gif)(\?.*)?$/,
-        use: [
-         {
-           loader:  'url-loader',
-           options: {
-            esModule:  false,
-            limit: 4096,
-            fallback: {
-              loader: 'file-loader',
-              options: {
-                name: 'static/img/[name].[hash:8].[ext]'
-              }
-            }
-          }
-         }
-        ],
-        type: 'javascript/auto'
+        type: 'asset/resource',
+        generator: {
+					filename: 'static/images/[name].[hash][ext][query]'
+				}
       }, 
       { // 加载 css
         test: /\.css$/,
@@ -99,6 +99,7 @@ module.exports = {
           /* config.module.rule('css').oneOf('normal') */
           {
             use: [
+              // 'vue-style-loader',
               {
                 loader: MiniCssExtractPlugin.loader
               },
@@ -125,8 +126,11 @@ module.exports = {
           /* config.module.rule('s(c|a)ss').oneOf('normal') */
           {
             use: [
+              // {
+              //   loader: MiniCssExtractPlugin.loader
+              // },
               {
-                loader: MiniCssExtractPlugin.loader
+                loader: 'vue-style-loader'
               },
               {
                 loader: 'css-loader',
@@ -154,21 +158,10 @@ module.exports = {
 
       { // 加载字体
         test: /\.(woff|woff2|eot|ttf|otf)(\?.*)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 4096,
-              fallback: {
-                loader: 'file-loader',
-                options: {
-                  name: 'static/fonts/[name].[hash:8].[ext]'
-                }
-              }
-            }
-          }
-        ],
-        type: 'javascript/auto'
+        type: 'asset/resource',
+        generator: {
+					filename: 'static/fonts/[name].[hash][ext][query]'
+				}
       }
     ]
   },
@@ -180,6 +173,19 @@ module.exports = {
     new FriendlyErrorsWebpackPlugin({}),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.ProgressPlugin(),
+    new webpack.DefinePlugin(
+      {
+        'process.env': {
+          // VUE_APP_BASE_API: '"/prod-api"',
+          // NODE_ENV: '"production"',
+          // BASE_URL: '"/"'
+        }
+      }
+    ),
+    new HtmlWebpackPlugin({ // 在生成环境会启用压缩
+      template: resolve('public/index.html'),
+      inject: 'body'
+    }),
     new MiniCssExtractPlugin(
       {
         filename: 'static/css/[name].[contenthash:8].css',
@@ -187,7 +193,6 @@ module.exports = {
         ignoreOrder: true
       }
     ),
-    /* config.plugin('copy') */
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -208,56 +213,9 @@ module.exports = {
     // vue: 'Vue'
   },
   optimization: {
-    // minimizer: [
-    //   {
-    //     options: {
-    //       test: /\.m?js(\?.*)?$/i,
-    //       chunkFilter: () => true,
-    //       warningsFilter: () => true,
-    //       extractComments: false,
-    //       sourceMap: false,
-    //       cache: true,
-    //       cacheKeys: defaultCacheKeys => defaultCacheKeys,
-    //       parallel: true,
-    //       include: undefined,
-    //       exclude: undefined,
-    //       minify: undefined,
-    //       terserOptions: {
-    //         output: {
-    //           comments: /^\**!|@preserve|@license|@cc_on/i
-    //         },
-    //         compress: {
-    //           arrows: false,
-    //           collapse_vars: false,
-    //           comparisons: false,
-    //           computed_props: false,
-    //           hoist_funs: false,
-    //           hoist_props: false,
-    //           hoist_vars: false,
-    //           inline: false,
-    //           loops: false,
-    //           negate_iife: false,
-    //           properties: false,
-    //           reduce_funcs: false,
-    //           reduce_vars: false,
-    //           switches: false,
-    //           toplevel: false,
-    //           typeofs: false,
-    //           booleans: true,
-    //           if_return: true,
-    //           sequences: true,
-    //           unused: true,
-    //           conditionals: true,
-    //           dead_code: true,
-    //           evaluate: true
-    //         },
-    //         mangle: {
-    //           safari10: true
-    //         }
-    //       }
-    //     }
-    //   }
-    // ],
+    minimizer: [
+      new CssMinimizerPlugin() // webpack@5 仅在生产环境开启 CSS 优化
+    ],
     splitChunks: {
       chunks: 'all',
       cacheGroups: {
--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
 
@@ -9,11 +8,6 @@ const baseConfig = require('./webpack.base.config')
 
 module.exports = merge(baseConfig, {
   mode: 'development',
-  output: {
-    filename: 'static/js/[name].js',
-    path: resolve('dist'),
-    publicPath: '/'
-  },
   devServer: {
       port: 9091,
       proxy: {
@@ -23,21 +17,7 @@ module.exports = merge(baseConfig, {
         }
       }
   },
-  devtool: 'source-map',
   plugins: [
-    /* config.plugin('define') */
-    new webpack.DefinePlugin(
-      {
-        'process.env': {
-          VUE_APP_BASE_API: '"/dev-api"',
-          NODE_ENV: '"development"',
-          BASE_URL: '"/"'
-        }
-      }
-    ),
-    new HtmlWebpackPlugin({
-      template: resolve('public/index.html'),
-      inject: 'body'
-    })
+
   ]
 })
